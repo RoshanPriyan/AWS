8adeff327715fdd9a779779466fd2fce174f5717
RoshanPriyan <priyanhari2303@gmail.com>
2024-10-05 17:14:34 +0530

    start stop and terminate ec2 instance

--- create_ec2_instance.py
+++ create_ec2_instance.py
@@ -9,3 +9,12 @@ response = ec2_console.run_instances(
     MaxCount=1,                       # Max number of instances
     MinCount=1,                       # Min number of instances
 )
+
+# ___ stop ec2 instance___
+aws_management_console = boto3.session.Session(profile_name="default", region_name="us-east-1")
+ec2_console = aws_management_console.client(service_name="ec2")
+
+# response = ec2_console.start_instances(InstanceIds=['i-00a69bcb512028f7d']) # start instance
+# response = ec2_console.start_instances(InstanceIds=['i-00a69bcb512028f7d']) # stop instance
+response = ec2_console.terminate_instances(InstanceIds=['i-00a69bcb512028f7d'])     # terminate instance
+
