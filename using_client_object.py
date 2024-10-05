5817e00019741088869a172928293722540765f2
RoshanPriyan <priyanhari2303@gmail.com>
2024-10-05 11:06:24 +0530

    using_client_object

--- using_client_object.py
+++ using_client_object.py
@@ -1,1 +1,8 @@
+import boto3
 
+aws_management_console = boto3.session.Session(profile_name="default")
+iam_console = aws_management_console.client(service_name="iam")
+
+user = iam_console.list_users()
+for i in user.get("Users"):
+    print(i.get("UserName"))
